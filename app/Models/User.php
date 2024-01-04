27bba9b76f8ff92b1060172a01f7130370869a16
Sean Gautschy <sean.gautschy@gmail.com>
2024-01-03 19:55:58 -0700

    user updates

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -42,4 +45,22 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+
+            get: fn ($value) => strtoupper($value)
+
+        );
+    }
+
+    protected function password(): Attribute
+    {
+        return Attribute::make(
+
+            set: fn ($value) => bcrypt($value)
+
+        );
+    }
 }
