ffd50349c48ccb19306537d7415421ab4762fe47
Sean Gautschy <sean.gautschy@gmail.com>
2024-01-07 15:35:32 -0700

    generate avatars on profile with openai

--- resources/views/profile/partials/user-avatar-form.blade.php
+++ resources/views/profile/partials/user-avatar-form.blade.php
@@ -3,12 +3,19 @@
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
             User Avatar
         </h2>
-        <img src="{{ "/storage/$user->avatar" }}" alt="user avatar" class="w-8 h-8 rounded-full">
-
-        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            Add or update user avatar
-        </p>
     </header>
+    
+    <img src="{{ "/storage/$user->avatar" }}" alt="user avatar" class="w-9 h-9 rounded-full">
+    <form action="{{ route('profile.avatar.ai') }}" method="post" class="mt-4">
+        @csrf
+        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+            Generate avatar from ai
+        </p>
+        <x-primary-button>Generate Avatar</x-primary-button>
+    </form>
+    <p class="my-4 text-sm text-gray-600 dark:text-gray-400">
+        or
+    </p>
     @if (session('message'))
         <div class="text-red-500" style="color: green">
             {{ session('message') }}
@@ -19,7 +26,7 @@
         @csrf
         @method('patch')
         <div>
-            <x-input-label for="name" value="Avatar" />
+            <x-input-label for="name" value="Upload Avatar from Computer" />
             <x-text-input id="avatar" name="avatar" type="file" class="mt-1 block w-full" :value="old('avatar', $user->avatar)" autofocus autocomplete="avatar" />
             <x-input-error class="mt-2" :messages="$errors->get('avatar')" />
         </div>
